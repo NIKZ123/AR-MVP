dfba0467d0e3838cff9750c9924688d2f6e8051d
nmpaul <nmpaul@deakin.edu.au>
2021-04-25 00:22:32 +1000

    changes to index.html

--- views/index.js
+++ views/index.js
@@ -8,9 +8,14 @@ loginButton.addEventListener("click", (e) => {
     const password = loginForm.password.value;
 
     if (username === "user" && password === "web_dev") {
-        alert("You have successfully logged in.");
-        location.reload();
+        // alert("You have successfully logged in.");
+        // myFunction();
+        window.location.href = "story.html";
     } else {
         loginErrorMsg.style.opacity = 1;
     }
 })
+
+// function myFunction() {
+//     location.replace("story.html")
+//   }
